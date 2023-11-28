d0a06c7c5b6677ad03c811c3d0590ad2955bcba6
Ikar <ikarpiatkowski@proton.me>
2023-11-28 16:31:45 +0100

    final

---
+++ src/providers/ModalProvider.tsx
@@ -0,0 +1,31 @@
+'use client';
+import { useEffect, useState } from 'react';
+import SubscribeModal from '@/components/SubscribeModal';
+import { ProductWithPrice } from '@/types';
+import AuthModal from '@/components/AuthModal';
+import UploadModal from '@/components/UploadModal';
+interface ModalProviderProps {
+  products: ProductWithPrice[];
+}
+
+const ModalProvider: React.FC<ModalProviderProps> = ({ products }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <>
+      <AuthModal />
+      <UploadModal />
+      <SubscribeModal products={products} />
+    </>
+  );
+};
+
+export default ModalProvider;
